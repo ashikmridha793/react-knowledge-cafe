1a05f7a8d5c46771ffb004658502b3738c974832
Ashikmridha793 <ashikmridha793@gmail.com>
2025-12-20 19:14:22 +0600

    added some header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      
-      <h1 className='text-6xl bg-red-300'>Knowledge Cafe</h1>
+    <Header></Header>
       
     </>
   )
